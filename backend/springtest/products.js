d30e1f34db5660f57393a3562465073eaf4b742f
therealnicklaus <nicklauswei@yahoo.com.sg>
2021-09-30 15:22:24 +0800

    database and changes

---
+++ backend/springtest/products.js
@@ -0,0 +1,30 @@
+[
+	{
+		"id": 1,
+		"title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+		"price": 109.95,
+		"description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+		"image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+		"qty": "50"
+		"category" : "3"
+	},
+	{
+		"id": 2,
+		"text": "Mens Casual Premium Slim Fit T-Shirts",
+		"price": 22.3,
+		"description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+		"image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+		"qty": "50"
+		"category" : "5"
+	},
+	{
+		"id": 3,
+		"text": "Mens Cotton Jacket",
+		"price": 55.99,
+		"description": "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+		"image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+		"qty": "50"
+		"category" : "3"
+	},
+
+]
